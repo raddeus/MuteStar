f50c366a60520bcc73342a373e056e06904a7887
Thad Blankenship <thadblankenship@gmail.com>
2014-06-06 01:50:31 -0500

    Initial Commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnToggleActive = new System.Windows.Forms.Button();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatusBar = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel6 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.notify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnToggleActive
+            // 
+            this.btnToggleActive.AutoSize = true;
+            this.btnToggleActive.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnToggleActive.BackColor = System.Drawing.Color.Crimson;
+            this.btnToggleActive.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnToggleActive.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnToggleActive.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnToggleActive.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnToggleActive.Font = new System.Drawing.Font("Verdana", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnToggleActive.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnToggleActive.Location = new System.Drawing.Point(0, 0);
+            this.btnToggleActive.Margin = new System.Windows.Forms.Padding(10);
+            this.btnToggleActive.Name = "btnToggleActive";
+            this.btnToggleActive.Padding = new System.Windows.Forms.Padding(10, 10, 10, 40);
+            this.btnToggleActive.Size = new System.Drawing.Size(234, 143);
+            this.btnToggleActive.TabIndex = 3;
+            this.btnToggleActive.Text = "Start";
+            this.btnToggleActive.UseVisualStyleBackColor = false;
+            this.btnToggleActive.Click += new System.EventHandler(this.btnToggleActive_Click);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(77, 25);
+            this.toolStripStatusLabel1.Text = "Status: ";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Overflow = System.Windows.Forms.ToolStripItemOverflow.Always;
+            this.lblStatus.Size = new System.Drawing.Size(112, 25);
+            this.lblStatus.Text = "Initializing...";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(42, 25);
+            this.toolStripStatusLabel2.Text = "Status:";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(70, 25);
+            this.toolStripStatusLabel3.Text = "Initializing...";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(118, 25);
+            this.toolStripStatusLabel4.Text = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Click += new System.EventHandler(this.toolStripStatusLabel4_Click);
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(118, 25);
+            this.toolStripStatusLabel5.Text = "toolStripStatusLabel5";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusStrip1.AutoSize = false;
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatusBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 110);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(954, 33);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatusBar
+            // 
+            this.lblStatusBar.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatusBar.Name = "lblStatusBar";
+            this.lblStatusBar.Size = new System.Drawing.Size(112, 28);
+            this.lblStatusBar.Text = "Initializing...";
+            // 
+            // toolStripStatusLabel6
+            // 
+            this.toolStripStatusLabel6.Name = "toolStripStatusLabel6";
+            this.toolStripStatusLabel6.Size = new System.Drawing.Size(70, 17);
+            this.toolStripStatusLabel6.Text = "Initializing...";
+            // 
+            // notify
+            // 
+            this.notify.BalloonTipText = "MuteStar is minimizing to the taskbar.";
+            this.notify.BalloonTipTitle = "MuteStar";
+            this.notify.ContextMenuStrip = this.contextMenuStrip1;
+            this.notify.Icon = ((System.Drawing.Icon)(resources.GetObject("notify.Icon")));
+            this.notify.Text = "MuteStar";
+            this.notify.Click += new System.EventHandler(this.notify_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(234, 143);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnToggleActive);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(192, 168);
+            this.Name = "MainForm";
+            this.Text = "MuteStar";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnToggleActive;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel6;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatusBar;
+        private System.Windows.Forms.NotifyIcon notify;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using CoreAudio;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MainForm : Form
+    {
+        private static IntPtr windowHandle = IntPtr.Zero;
+        private static int windowPid = 0;
+        private static bool active = false;
+        private static bool? windowActivePrev = null;
+        private static MMDeviceEnumerator DevEnum;
+        private static MMDevice device;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
+
+        public MainForm()
+        {
+            DevEnum = new MMDeviceEnumerator();
+            device = DevEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+            InitializeComponent();
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width,
+                                      workingArea.Bottom - Size.Height);
+            lblStatusBar.Text = "Ready | Stopped";
+        }
+
+        private void start()
+        {
+            active = true;
+            btnToggleActive.Text = "Stop";
+            lblStatusBar.Text = "Starting...";
+            btnToggleActive.BackColor = Color.FromArgb(43, 189, 121);
+            btnToggleActive.FlatAppearance.MouseOverBackColor = Color.FromArgb(32, 201, 103);
+            
+            timer1.Start();
+        }
+        private void stop()
+        {
+            active = false;
+            btnToggleActive.Text = "Start";
+            btnToggleActive.BackColor = Color.FromArgb(214, 41, 49);
+            btnToggleActive.FlatAppearance.MouseOverBackColor = Color.FromArgb(186, 17, 25);
+            lblStatusBar.Text = "Ready | Stopped";
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (windowHandle != IntPtr.Zero)
+            {
+                IntPtr currentForegroundWindow = GetForegroundWindow();
+                if (currentForegroundWindow == windowHandle)
+                {
+                    lblStatusBar.Text = "Running | Game window active.";
+                    if (windowActivePrev == null || windowActivePrev == false)
+                    {
+                        windowActivePrev = true;
+                        lblStatusBar.Text = "Turning Volume On";
+                        setMute(false, (uint)windowPid);
+                        this.Icon = MuteStar.Properties.Resources.Speaker;
+                    }
+                }
+                else
+                {
+                    lblStatusBar.Text = "Running | Game window inactive.";
+                    if (windowActivePrev != null && windowActivePrev == true)
+                    {
+                        windowActivePrev = false;
+                        lblStatusBar.Text = "Turning Volume Off";
+                        setMute(true, (uint)windowPid);
+                        this.Icon = MuteStar.Properties.Resources.Mute;
+                    }
+                }
+            }
+            else
+            {
+                lblStatusBar.Text = "Searching for wildstar window...";
+                IntPtr handle;
+                int pid;
+                if (findWindow(out handle, out pid))
+                {
+                    windowPid = pid;
+                    windowHandle = handle;
+                   // start();
+                }
+            }
+        }
+
+        private bool findWindow(out IntPtr handle, out int pid)
+        {
+            Process[] pList = Process.GetProcesses();
+            foreach (Process p in pList)
+            {
+                if (p.MainWindowTitle.Contains("WildStar ") && p.MainWindowTitle.Length == 13)
+                {
+                    handle = p.MainWindowHandle;
+                    GetWindowThreadProcessId(handle, out pid);
+                    return true;
+                }
+            }
+            handle = IntPtr.Zero;
+            pid = 0;
+            return false;
+        }
+
+        private void setMute(bool mute, uint windowPid)
+        {
+            for (int i = 0; i < device.AudioSessionManager2.Sessions.Count; i++)
+            {
+                AudioSessionControl2 session = device.AudioSessionManager2.Sessions[i];
+                uint pid = session.GetProcessID;
+                AudioMeterInformation mi = session.AudioMeterInformation;
+                SimpleAudioVolume vol = session.SimpleAudioVolume;
+                if (pid == windowPid)
+                {
+                    vol.Mute = mute;
+                }
+            }
+        }
+
+        private void btnToggleActive_Click(object sender, EventArgs e)
+        {
+            if (active == true)
+            {
+                stop();
+            }
+            else
+            {
+                start();
+            }
+        }
+
+        private void toolStripStatusLabel4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notify.Visible = true;
+                notify.ShowBalloonTip(500);
+                this.Hide();
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notify.Visible = false;
+            }
+        }
+        private void notify_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (System.Windows.Forms.Application.MessageLoop)
+            {
+                // WinForms app
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                // Console app
+                System.Environment.Exit(1);
+            }
+        }
+    }
+}
